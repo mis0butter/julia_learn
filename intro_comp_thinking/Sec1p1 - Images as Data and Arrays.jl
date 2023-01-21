de99d2b700f0ccd56b6c0de7f4a657665467e0ea
Junette <junetter@gmail.com>
2023-01-20 20:01:18 -0600

    omg these Julia packages are ridiculous. creating optimization notebook

--- intro_comp_thinking/Sec1p1 - Images as Data and Arrays.jl
+++ intro_comp_thinking/Sec1p1 - Images as Data and Arrays.jl
@@ -3,3 +3,17 @@ begin
     using PlutoUI 
     using HypertextLiteral 
 end 
+
+## use Images.jl to load an image file 
+
+# Step 1: specify URL 
+url = "https://user-images.githubusercontent.com/6933510/107239146-dcc3fd00-6a28-11eb-8c7b-41aaf6618935.png" 
+
+# Step 2: download 
+philip_filename = download(url) # download to a local file. The filename is returned 
+
+# Step 3: load file 
+philip = load(philip_filename)
+
+## Exercise: change URL 
+## Exercise: download a file already on your computer 
