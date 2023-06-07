b10f934e5f82cce8d9c05e675104cac0b9491b58
mis0butter <junetter@gmail.com>
2023-06-07 00:06:52 -0500

    have results for predator_prey, plots are looking pretty good actually

--- test/lorenz_test.jl
+++ test/lorenz_test.jl
@@ -25,10 +25,16 @@ using Random, Distributions
 # choose ODE, plot states --> measurements 
 
 #  
-fn          = ode_sine 
+fn          = predator_prey 
 plot_option = 1 
 t, x, dx_true, dx_fd = ode_states(fn, plot_option) 
 
+# split into training and validation data 
+train_fraction = 0.7 
+t_train, t_test             = split_train_test(t, train_fraction) 
+x_train, x_test             = split_train_test(x, train_fraction) 
+dx_true_train, dx_true_test = split_train_test(dx_true, train_fraction) 
+dx_fd_train, dx_fd_test     = split_train_test(dx_fd, train_fraction) 
 
 ## ============================================ ##
 # SINDy alone 
@@ -39,16 +45,6 @@ poly_order = n_vars
 
 Ξ_fd   = SINDy_c_recursion(x, dx_fd, 0, λ, poly_order ) 
 Ξ_true = SINDy_c_recursion(x, dx_true, 0, λ, poly_order ) 
-
-
-## ============================================ ##
-# split into training and validation data 
-
-train_fraction = 0.7 
-t_train, t_test             = split_train_test(t, train_fraction) 
-x_train, x_test             = split_train_test(x, train_fraction) 
-dx_true_train, dx_true_test = split_train_test(dx_true, train_fraction) 
-dx_fd_train, dx_fd_test     = split_train_test(dx_fd, train_fraction) 
 
 
 ## ============================================ ##
@@ -70,39 +66,42 @@ display(z_fd)
 ## ============================================ ##
 # generate + validate data 
 
-dx_fn = build_dx_fn(poly_order, z_fd) 
-t_val, x_val = validate_data(t_test, x_test, dx_fn)
+dx_gpsindy_fn = build_dx_fn(poly_order, z_fd) 
+dx_sindy_fn  = build_dx_fn(poly_order, Ξ_fd)
 
-
-## ============================================ ##
-
-
-tspan = (t_test[1], t_test[end])
-prob = ODEProblem(dx_fn, x0, tspan)
-
-# solve the ODE
-sol = solve(prob,  reltol = 1e-8, abstol = 1e-8)
-x_validate = sol.u ; 
-x_validate = mapreduce(permutedims, vcat, x_validate) 
-t_validate = sol.t 
-
-## ============================================ ##
-
+t_gpsindy_val, x_gpsindy_val = validate_data(t_test, x_test, dx_gpsindy_fn)
+t_sindy_val, x_sindy_val     = validate_data(t_test, x_test, dx_sindy_fn)
 
 # display training data 
 plot(t_train, x_train, 
-    lw = 2, 
+    lw = 3, 
+    c = :black, 
     label = "train", 
-    grid = false ) 
+    grid = false, 
+    xlim = (t_train[end]*3/4, t_test[end]), 
+    legend 
+    # ylim = ( min(x_test[1], x_test[end]), max( x_test[1], x_test[end] ) )
+    ) 
 plot!(t_test, x_test, 
     ls = :dash, 
+    c = :blue,
+    lw = 3,  
+    label = "test" 
+    )
+plot!(t_gpsindy_val, x_gpsindy_val, 
+    ls = :dashdot, 
+    lw = 1.5, 
     c = :red, 
-    label = "test" )
-plot!(t_val, x_val, 
+    label = "GP SINDy" 
+    )
+plot!(t_sindy_val, x_sindy_val, 
     ls = :dot, 
+    lw = 1.5, 
     c = :green, 
-    label = "validate" )
+    label = "SINDy" 
+    )
 
+## ============================================ ##
 
 
 
