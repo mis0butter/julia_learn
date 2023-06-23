a40ec172145f8f2ab002d8ecde47272b1bc23719
mis0butter <junetter@gmail.com>
2023-06-23 04:40:34 -0700

    committing code that can now generate metrics printout and plots

--- src/init_params.jl
+++ src/init_params.jl
@@ -12,14 +12,14 @@ function init_params(fn)
         str = "ode_sine" 
     elseif fn == predator_prey 
         # x0  = [ 1.0; 0.5 ] 
-        # x0  = 10*round.( rand(2), digits=2 )
-        x0  = 0.5 .+ 0.25*rand(2) 
-        println("x0 = ", x0)
+        x0  = 10*round.( rand(2), digits=2 )
+        # x0  = 0.5 .+ 0.25*rand(2) 
+        # println("x0 = ", x0)
         str = "predator_prey" 
     end 
-    p      = [ 10.0, 28.0, 8/3, 2.0 ] 
+    # p      = [ 10.0, 28.0, 8/3, 2.0 ] 
     # p      = [ 1.1, 0.4, 1, 0.4 ] 
-    # p      = [ 1.5, 1, 3, 1 ] 
+    p      = [ 1.5, 1, 3, 1 ] 
     n_vars = size(x0, 1) 
     tf     = 14.3 
     ts     = (0.0, tf) 
--- test/predator_prey_test.jl
+++ test/predator_prey_test.jl
@@ -49,7 +49,7 @@ println("λ = ", λ)
 # finite difference 
 hist_fd = Hist( [], [], [], [], [] ) 
 @time z_gpsindy, hist_fd = sindy_gp_admm( x_train, dx_fd_train, λ, hist_fd ) 
-display(z_gpsindy) 
+# display(z_gpsindy) 
 
 
 ## ============================================ ## 
@@ -67,41 +67,40 @@ plot_prey_predator( t_train, x_train, t_test, x_test, t_sindy_val, x_sindy_val, 
 ## ============================================ ##
 # print stats 
 
-# print some coeff stats 
-coeff_norm = [ opnorm( Ξ_true - Ξ_sindy ) , opnorm( Ξ_true - z_gpsindy ) ] 
-println("COEFFICIENTS")
-println("  opnorm( Ξ_true - Ξ_sindy ) = \n    ", coeff_norm[1] )
-println("  opnorm( Ξ_true - z_gpsindy ) = \n    ", coeff_norm[2] )
+if isequal( t_test[end], t_sindy_val[end] ) && isequal( t_test[end], t_gpsindy_val[end] )
 
-# print some predicted stats 
-valid_norm = zeros(1, 4) 
-println("VALIDATION DATA")
+    # print some coeff stats 
+    coeff_norm = [ opnorm( Ξ_true - Ξ_sindy ) , opnorm( Ξ_true - z_gpsindy ) ] 
 
-    println("  i = ", i) 
+    # print some predicted stats 
+    valid_norm = zeros(1, 4) 
+
     i = 1 
     # true - SINDy 
     valid_norm[1,1] = norm( x_test[:,i] - x_sindy_val[:,i] ) 
-    println("    opnorm( x_true - x_sindy ) = \n    ", valid_norm[1,1] )
 
     # true - GP SINDy
     valid_norm[1,2] = norm( x_test[:,i] - x_gpsindy_val[:,i] )
-    println("    opnorm( x_true - x_gpsindy ) = \n    ", valid_norm[1,2] )     
 
-    println("  i = ", 2) 
     i = 2 
     # true - SINDy 
     valid_norm[1,3] = norm( x_test[:,i] - x_sindy_val[:,i] ) 
-    println("    opnorm( x_true - x_sindy ) = \n    ", valid_norm[1,3] )
 
     # true - GP SINDy
     valid_norm[1,4] = norm( x_test[:,i] - x_gpsindy_val[:,i] )
-    println("    opnorm( x_true - x_gpsindy ) = \n    ", valid_norm[1,4] )     
 
     println("x0 = ", round.(x0, digits = 2))
 
     # for displaying in table 
+    println("truth - sindy, truth - gpsindy dynamics = ")
     table_norm = round.([ coeff_norm[1] valid_norm[1] valid_norm[2] coeff_norm[2] valid_norm[3]  valid_norm[4] ], digits = 2)
+    display(table_norm) 
 
+else
+
+    println("poop something diverged" )
+
+end 
 
 ## ============================================ ##
 # save fig 
